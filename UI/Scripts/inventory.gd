9b35f9770f38dca5da1eee795f5c875da6f743ec
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2024-01-21 21:20:11 +1000

    Thinking about drag and drop

--- UI/Scripts/inventory.gd
+++ UI/Scripts/inventory.gd
@@ -1,17 +1,6 @@
 extends CanvasLayer
 
 
-func _process(delta):
-	show_player(player.get_node("AnimatedSprite2D"))
-
 func _input(event):
 	if event.is_action_pressed("inventory"):
 		visible = !visible
-
-@onready var player: Player = $"../Player"
-@onready var avatar = $TextureRect/TextureRect2/Avatar
-func show_player(sprite: AnimatedSprite2D):
-	var animation = sprite.animation
-	var frame = sprite.frame
-	var current_frame = sprite.sprite_frames.get_frame_texture(animation, frame)
-	avatar.texture = current_frame
--- UI/Scripts/slot.gd
+++ UI/Scripts/slot.gd
@@ -1,16 +1,25 @@
-extends MarginContainer
+extends Panel
 class_name Slot
 
-@onready var texture: TextureRect = $SlotImage/TextureRect
-@onready var label_amount: Label = $SlotImage/Amount
+@onready var texture: TextureRect = $Texture
+@onready var label_amount: Label = $Amount
+const empty_style = Color8(255, 255, 255, 145)
+const defualt_style = Color8(255, 255, 255, 255)
+
+func _ready():
+	set_empty_style()
+
 
 func add_item(item_texture: Texture2D, amount: int):
 	texture.texture = item_texture
 	label_amount.text = str(amount)
-	
+	set_default_style()
+
+
 func clear_slot():
 	texture.texture = null
 	label_amount.text = ""
+	set_empty_style()
 
 func curent_item():
 	return [texture, label_amount]
@@ -27,6 +36,12 @@ func update_info(new_amount: int):
 		label_amount.visible = false
 	else: 
 		label_amount.visible = true
+
+func set_empty_style():
+	modulate = empty_style
+	
+func set_default_style():
+	modulate = defualt_style
 
 func show_status():
 	print_debug(texture.texture)
